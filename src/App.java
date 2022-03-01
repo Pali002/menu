adaebc112cf2bc5d87f1847058fad4659bc967f5
pali002 <zentaipali@gmail.com>
2022-03-01 09:19:01 +0100

    Program

--- src/App.java
+++ src/App.java
@@ -4,19 +4,33 @@ class App {
     public static void main(String[] args) throws Exception {
         Scanner sc = new Scanner(System.in);
 
-        
-        System.out.println("-------------------");
-        System.out.println();
-        System.out.println("1) Program indítása");
-        System.out.println();
-        System.out.println("2) Névjegy");
-        System.out.println();
-        System.out.println("3) Kilépés");
+        String valasztott = "";
 
-        System.out.println();
-        System.out.println();
-        System.out.println("Valasztas:");
-        String valasztott = sc.nextLine();
+        do{
+            System.out.println("-------------------");
+            System.out.println();
+            System.out.println("1) Program indítása");
+            System.out.println();
+            System.out.println("2) Névjegy");
+            System.out.println();
+            System.out.println("3) Kilépés");
+    
+            System.out.println();
+            System.out.println();
+            System.out.println("Valasztas:");
+            valasztott = sc.nextLine();
+            switch(valasztott){
+                case "1":
+                    System.out.println("Program...");
+                    break;
+                case "2":
+                    System.out.println("Névjegy...");
+                    break;
+                case "3":
+                System.out.println("Kilépés...");
+                break;
+            };    
+        }while(!valasztott.equals("3"));
         
 
         sc.close();
